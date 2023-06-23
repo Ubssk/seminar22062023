d893de6810c689810c5fff118a067ddc7aab0607
Anna <99716@mail.ru>
2023-06-23 14:10:53 +0300

    задача2

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+if (firstDigit > secondDigit)
+{    
+    Console.WriteLine("Число " + firstDigit + " больше");
+}
+else
+{    
+    Console.WriteLine("Число " + secondDigit + " больше");
+}
